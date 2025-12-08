1c3790bf219b6aa7c99d8cafab2c9fc904fb421c
kvkvkvkvkv <33504944+kvkvkvkvkv@users.noreply.github.com>
2025-12-08 15:09:55 +0530

    Time: 6 ms (25.11%) | Memory: 47.2 MB (11.55%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -4,17 +4,14 @@ class Solution {
         Map<Integer, Set<Character>> cols = new HashMap();
         Map<Integer, Set<Character>> cell = new HashMap();
 
-        for(int i=0;i<9;i++) {
-            rows.put(i, new HashSet());
-            cols.put(i, new HashSet());
-            cell.put(i, new HashSet());
-        }
-
         for(int r=0;r<9;r++) {
             for(int c=0;c<9;c++) {
                 char cur = board[r][c];
+                int val = ((r/3)*3)+(c/3);
+                rows.putIfAbsent(r, new HashSet());
+                cols.putIfAbsent(c, new HashSet());
+                cell.putIfAbsent(val, new HashSet());
                 if(cur != '.') {
-                    int val = ((r/3)*3)+(c/3);
                     if(rows.get(r).contains(cur) || cols.get(c).contains(cur) || 
                     cell.get(val).contains(cur)) {
                         return false;
