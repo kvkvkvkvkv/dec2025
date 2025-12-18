14ee1859b9830fc526c88c0d2bc6add173b1f664
kvkvkvkvkv <33504944+kvkvkvkvkv@users.noreply.github.com>
2025-12-18 09:36:58 +0530

    Time: 29 ms (11.55%) | Memory: 50.3 MB (16.70%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        
+        Map<Integer, Set<Pair<Integer,Integer>>> map = new HashMap();
+
+        for(int[] ele:times) {
+            map.computeIfAbsent(ele[0],i-> new HashSet<>()).add(new Pair(ele[1],ele[2]));
+        }
+
+        Queue<Pair<Integer,Integer>> q = new LinkedList();
+        q.add(new Pair(k,0));
+        int[] time = new int[n+1];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        time[k] = 0;
+        while(!q.isEmpty()) {
+            Pair<Integer,Integer> top = q.poll();
+            for(var next:map.getOrDefault(top.getKey(),new HashSet<>())) {
+                if(time[next.getKey()]>top.getValue()+next.getValue()) {
+                    time[next.getKey()] = top.getValue()+next.getValue();
+                    q.add(new Pair(next.getKey(),time[next.getKey()]));
+                }
+            }
+        }
+
+        int max = -1;
+        for(int index=1;index<=n;index++) {
+            max = Math.max(max, time[index]);
+        }
+        return max==Integer.MAX_VALUE?-1:max;
+    }
+}
