3b529851ea1fa579f2c9ec4ac3a8b642b13a427c
kvkvkvkvkv <33504944+kvkvkvkvkv@users.noreply.github.com>
2025-12-21 21:54:45 +0530

    Time: 447 ms (13.08%) | Memory: 44 MB (13.78%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,22 @@
+class Solution {
+    Boolean[][] memo;
+    public boolean isMatch(String s, String p) {
+        memo = new Boolean[s.length()+1][p.length()+1];
+        return match(s,p,0,0);
+    }
+
+    public boolean match(String s, String p, int i1, int i2) {
+        if(i2 == p.length()) {
+            return i1 == s.length();
+        }
+        
+        boolean firstMatch = (i1 < s.length() &&
+                (p.charAt(i2) == s.charAt(i1) || p.charAt(i2) == '.'));        
+
+        if(i2+1 < p.length() && p.charAt(i2+1) == '*') {
+            return match(s,p,i1,i2+2) || firstMatch && match(s,p,i1+1,i2);
+        }
+
+        return firstMatch && match(s,p,i1+1,i2+1);
+    }
+}
