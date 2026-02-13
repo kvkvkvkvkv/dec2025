f8362354509bbf9238e753c29961d110b337db22
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-13 17:37:27 +0530

    Time: 7 ms (36.17%) | Memory: 48 MB (20.38%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,44 +1,38 @@
 class Solution {
-    boolean[] stack;
-    boolean[] seen;
-    int[] ans;
-    int k;
-    public int[] findOrder(int numCourses, int[][] pre) {
-        stack = new boolean[numCourses];
-        seen = new boolean[numCourses];
-        ans = new int[numCourses];
-        k = numCourses-1;
-        Map<Integer,Set<Integer>> map = new HashMap();
-        for(int[] ele:pre) {
-            map.computeIfAbsent(ele[1], i -> new HashSet<>()).add(ele[0]);
-        }
+    int[] inward;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        inward = new int[numCourses];
 
+        for(int[] ele:prerequisites) {
+            inward[ele[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int[] ele:prerequisites) {
+            map.computeIfAbsent(ele[1], key->new ArrayList<>()).add(ele[0]);
+        }
         for(int i=0;i<numCourses;i++) {
-            if(cycle(map,i)){
-                return new int[]{};
+            if(inward[i] == 0) {
+                q.add(i);
             }
         }
-        return ans;
-    }
-
-    boolean cycle(Map<Integer,Set<Integer>> map, int ele) {
-        if(stack[ele]) {
-            return true;
-        }
-
-        if(seen[ele]) {
-            return false;
-        }
-
-        seen[ele] = true;
-        stack[ele] = true;
-        for(int val:map.getOrDefault(ele, new HashSet<>())) {
-            if(cycle(map, val)) {
-                return true;
+        int[] ans = new int[numCourses];
+        int index=0;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            ans[index++] = cur;
+            if(map.get(cur) == null) {
+                continue;
+            }
+            for(int ele:map.get(cur)) {
+                inward[ele]--;
+                if(inward[ele] == 0) {
+                    q.add(ele);
+                }
             }
         }
-        ans[k--] = ele;
-        stack[ele]=false;
-        return false;
+
+        return index==numCourses?ans:new int[]{};
     }
 }
