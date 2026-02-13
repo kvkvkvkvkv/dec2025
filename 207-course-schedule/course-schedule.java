191fda691f636855474cfa144674225c195c4233
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-13 17:22:23 +0530

    Time: 6 ms (75.34%) | Memory: 47.6 MB (14.14%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,41 @@
 class Solution {
-    int[] state;
-    public boolean canFinish(int numCourses, int[][] pre) {
-        state = new int[numCourses];
-        Map<Integer,Set<Integer>> map = new HashMap();
-        for(int[] ele:pre) {
-            map.computeIfAbsent(ele[1], i -> new HashSet<>()).add(ele[0]);
+    boolean[] stack;
+    boolean[] seen;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        stack = new boolean[numCourses];
+        seen = new boolean[numCourses];
+
+        Map<Integer, List<Integer>> map = new HashMap();
+
+        for(int[] ele:prerequisites) {
+            map.computeIfAbsent(ele[1], key -> new ArrayList<>()).add(ele[0]);
         }
-        Set<Integer> seen = new HashSet();
+
         for(int i=0;i<numCourses;i++) {
-            if(cycle(map,i)){
+            if(!seen[i] && cycle(map, i)) {
                 return false;
             }
         }
         return true;
     }
 
-    boolean cycle(Map<Integer,Set<Integer>> map, int ele) {
-        
-        if(state[ele]==2) {
-            return false;
-        }
-
-        if(state[ele]==1) {
+    boolean cycle(Map<Integer,List<Integer>> map, int node) {
+        if(stack[node]) {
             return true;
         }
 
-        state[ele]=1;
-        for(int val:map.getOrDefault(ele, new HashSet<>())) {
-            if(cycle(map,val)) {
+        if(map.get(node) == null || seen[node]) {
+            return false;
+        }
+
+        stack[node] = true;
+        seen[node]=true;
+        for(int next:map.get(node)) {
+            if(cycle(map, next)) {
                 return true;
             }
         }
-        state[ele]=2;
+        stack[node]=false;
         return false;
     }
 }
