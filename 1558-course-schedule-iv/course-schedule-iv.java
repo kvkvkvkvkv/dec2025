4a6fdaaf536ad004011df4e015204625617ab348
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-14 08:21:58 +0530

    Time: 163 ms (18.75%) | Memory: 53.4 MB (8.81%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -0,0 +1,34 @@
+class Solution {
+    Map<Pair<Integer,Integer>,Boolean> memo = new HashMap<>();
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        
+        Map<Integer,List<Integer>> map = new HashMap();
+        for(int[] ele:prerequisites) {
+            map.computeIfAbsent(ele[0], k -> new ArrayList()).add(ele[1]);
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+        for(int[] ele:queries) {
+            ans.add(isReach(ele[0],ele[1],map));
+        }
+        return ans;
+    }
+
+    boolean isReach(int start, int end, Map<Integer,List<Integer>> map) {
+        if(start == end) {
+            return true;
+        }
+        Pair<Integer,Integer> p = new Pair(start,end);
+        if(memo.get(p) != null) {
+            return memo.get(p);
+        }
+        for(int ele:map.getOrDefault(start, new ArrayList<>())) {
+            if(isReach(ele, end, map)) {    
+                return true;
+            }
+        }
+
+        memo.put(p, false);
+        return false;
+    }
+}
