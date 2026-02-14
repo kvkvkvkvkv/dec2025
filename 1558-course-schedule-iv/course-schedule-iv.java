2534336abf65642ae41660a8fbe09125dd20a6fe
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-14 09:13:24 +0530

    Time: 52 ms (40.27%) | Memory: 51.2 MB (37.81%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,39 +1,26 @@
 class Solution {
-    Map<Pair<Integer,Integer>,Boolean> memo = new HashMap<>();
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        
-        Map<Integer,List<Integer>> map = new HashMap();
+        boolean[][] matrix = new boolean[numCourses][numCourses]; 
         for(int[] ele:prerequisites) {
-            map.computeIfAbsent(ele[0], k -> new ArrayList()).add(ele[1]);
+            matrix[ele[0]][ele[1]] = true;
         }
-
-        for(int i = 0; i < numCourses; i++) {
-            isReach(i, i, map);
-        }
-
+        floyd(matrix);
         List<Boolean> ans = new ArrayList<>();
-        for(int[] ele:queries) {
-            ans.add(isReach(ele[0],ele[1],map));
+        for(int[] q:queries){
+            ans.add(matrix[q[0]][q[1]]);
         }
+
         return ans;
     }
 
-    boolean isReach(int start, int end, Map<Integer,List<Integer>> map) {
-        if(start == end) {
-            return true;
-        }
-        Pair<Integer,Integer> p = new Pair(start,end);
-        if(memo.get(p) != null) {
-            return memo.get(p);
-        }
-        for(int ele:map.getOrDefault(start, new ArrayList<>())) {
-            if(isReach(ele, end, map)) {    
-                memo.put(p, true);
-                return true;
+    void floyd(boolean[][] matrix) {
+        int n = matrix.length;
+        for(int via=0;via<n;via++) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    matrix[i][j] = (matrix[i][via] && matrix[via][j]) || matrix[i][j];
+                }
             }
         }
-
-        memo.put(p, false);
-        return false;
     }
 }
