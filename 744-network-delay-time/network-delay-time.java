77e9c561227256a3f667e99a2e5eec448b6ee18f
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-22 22:32:14 +0530

    Time: 17 ms (28.58%) | Memory: 50.3 MB (21.99%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,31 +1,39 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        
-        Map<Integer, Set<Pair<Integer,Integer>>> map = new HashMap();
+        Queue<int[]> q = new LinkedList<>();
+
+        Map<Integer,List<Pair<Integer,Integer>>> map = new HashMap();
 
         for(int[] ele:times) {
-            map.computeIfAbsent(ele[0],i-> new HashSet<>()).add(new Pair(ele[1],ele[2]));
+            map.computeIfAbsent(ele[0], key -> new ArrayList<>()).add(new Pair(ele[1], ele[2]));
         }
 
-        PriorityQueue<Pair<Integer,Integer>> q = new PriorityQueue<>((a,b) -> Integer.compare(a.getValue(),b.getValue()));
-        q.add(new Pair(k,0));
-        int[] time = new int[n+1];
-        Arrays.fill(time, Integer.MAX_VALUE);
-        time[k] = 0;
-        while(!q.isEmpty()) {
-            Pair<Integer,Integer> top = q.poll();
-            for(var next:map.getOrDefault(top.getKey(),new HashSet<>())) {
-                if(time[next.getKey()]>top.getValue()+next.getValue()) {
-                    time[next.getKey()] = top.getValue()+next.getValue();
-                    q.add(new Pair(next.getKey(),time[next.getKey()]));
+        q.add(new int[]{k,0});
+        int[] min = new int[n+1];
+        Arrays.fill(min, Integer.MAX_VALUE);
+        min[k]=0;
+        int count = 0;
+        while (!q.isEmpty()) {
+            int[] top = q.poll();
+            if(map.get(top[0]) == null) {
+                continue;
+            }
+            for(Pair<Integer,Integer> ele:map.get(top[0])) {
+                int key = ele.getKey();
+                int val = ele.getValue();
+
+                if(val+top[1]<min[key]) {
+                    min[key] = val+top[1];
+                    q.add(new int[]{key,min[key]});
                 }
             }
         }
 
-        int max = -1;
-        for(int index=1;index<=n;index++) {
-            max = Math.max(max, time[index]);
+        int m = -1;
+        for(int i=1;i<=n;i++) {
+            m = Math.max(min[i],m);
         }
-        return max==Integer.MAX_VALUE?-1:max;
+
+        return m==Integer.MAX_VALUE?-1:m;
     }
 }
