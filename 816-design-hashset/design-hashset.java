922818cb8b2fa65772d6081bee5ecdf0893caa7e
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-03-04 09:46:01 +0530

    Time: 17 ms (31.42%) | Memory: 52.9 MB (47.04%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,19 +1,63 @@
 class MyHashSet {
-    boolean[] val;
+    Bucket[] buckets;
     public MyHashSet() {
-        val = new boolean[(int)1e6+1];
+        buckets = new Bucket[500];
     }
     
     public void add(int key) {
-        val[key] = true;
+        int idx = key % 500;
+        if(buckets[idx] == null) {
+            buckets[idx] = new Bucket();
+        }
+        buckets[idx].put(key);
     }
     
     public void remove(int key) {
-        val[key] = false;
+        int idx = key % 500;
+        if (buckets[idx] == null) {
+            return;
+        }
+        buckets[idx].remove(key);
+    
     }
     
     public boolean contains(int key) {
-        return val[key];
+        int idx = key % 500;
+        if (buckets[idx] == null) {
+            return false;
+        }
+        return buckets[idx].contains(key);
+    }
+}
+
+class Bucket {
+    LinkedList<Integer> values;
+
+    Bucket() {
+        values = new LinkedList<>();
+    }
+
+    public int put(int value) {
+        if (!values.contains(value)) {
+            values.add(value);
+            return value;
+        }
+        return -1;
+    }
+
+    public boolean contains(int value) {
+        if (!values.contains(value)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int remove(int value) {
+        if (!values.contains(value)) {
+            return -1;
+        }
+        values.remove(Integer.valueOf(value));
+        return value;
     }
 }
 
