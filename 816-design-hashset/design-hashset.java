eaeb606a3cb70a48c15ce9cb84a85c43c37afc9b
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-03-04 15:48:18 +0530

    Time: 14 ms (56.64%) | Memory: 49.7 MB (94.97%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,15 +1,47 @@
+// class MyHashSet {
+//     Bucket[] buckets;
+//     public MyHashSet() {
+//         buckets = new Bucket[500];
+//     }
+    
+//     public void add(int key) {
+//         int idx = key % 500;
+//         if(buckets[idx] == null) {
+//             buckets[idx] = new Bucket();
+//         }
+//         buckets[idx].put(key);
+//     }
+    
+//     public void remove(int key) {
+//         int idx = key % 500;
+//         if (buckets[idx] == null) {
+//             return;
+//         }
+//         buckets[idx].remove(key);
+    
+//     }
+    
+//     public boolean contains(int key) {
+//         int idx = key % 500;
+//         if (buckets[idx] == null) {
+//             return false;
+//         }
+//         return buckets[idx].contains(key);
+//     }
+// }
+
 class MyHashSet {
-    Bucket[] buckets;
+    BSTBucket[] buckets;
     public MyHashSet() {
-        buckets = new Bucket[500];
+        buckets = new BSTBucket[500];
     }
     
     public void add(int key) {
         int idx = key % 500;
         if(buckets[idx] == null) {
-            buckets[idx] = new Bucket();
+            buckets[idx] = new BSTBucket();
         }
-        buckets[idx].put(key);
+        buckets[idx].insert(key);
     }
     
     public void remove(int key) {
@@ -17,7 +49,7 @@ class MyHashSet {
         if (buckets[idx] == null) {
             return;
         }
-        buckets[idx].remove(key);
+        buckets[idx].delete(key);
     
     }
     
@@ -59,6 +91,111 @@ class Bucket {
         values.remove(Integer.valueOf(value));
         return value;
     }
+}
+
+class TreeNode {
+    int key;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int key) {
+        this.key = key;
+        left = null;
+        right = null;
+    }
+}
+class BSTBucket {
+    TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+            return;
+        }
+        root = insertInBST(root, value);
+    }
+
+    public TreeNode insertInBST(TreeNode root, int value) {
+        if(root == null) {
+            return new TreeNode(value);
+        }
+        if(root.key>value) {
+            root.left = insertInBST(root.left, value);
+        } else if(root.key<value){
+            root.right = insertInBST(root.right, value);
+        } 
+        return root;
+    }
+
+    public boolean contains(int value) {
+        return contains(root, value);
+    }
+
+    public boolean contains(TreeNode root, int value) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.key == value) {
+            return true;
+        }
+        if(root.key>value) {
+            if(contains(root.left, value)) {
+                return true;
+            }
+        } else {
+            if(contains(root.right, value)) {
+                return true;
+            }
+        } 
+        return false;
+    }
+
+    public void delete(int value) {
+        if (root == null) {
+            return;
+        }
+        root = deleteInBST(root, value);
+    }
+
+    private TreeNode deleteInBST(TreeNode root, int value) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.key>value) {
+            TreeNode child = deleteInBST(root.left, value);
+            root.left = child;
+        } else if(root.key < value){
+            TreeNode child = deleteInBST(root.right, value);
+            root.right = child;
+        } else {
+            return delete(root);
+        }
+        return root;
+    }
+
+    TreeNode delete(TreeNode child) {
+        if(child.left == null && child.right == null) {
+            return null;
+        }
+        if(child.left == null) {
+            return child.right;
+        }
+        if(child.right == null) {
+            return child.left;
+        }
+        TreeNode newRoot = child.right;
+        TreeNode right = child.right;
+        while(right.left != null) {
+            right = right.left;
+        }
+        right.left = child.left;
+        child.right = null;
+        child.left = null;
+        return newRoot;
+    }
+
 }
 
 /**
