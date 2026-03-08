fd2780bec1659d435fb05f5d110ae816ea096b5c
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-03-08 06:18:28 +0530

    Time: 34 ms (22.16%) | Memory: 47.4 MB (5.70%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -3,34 +3,34 @@ class Solution {
         Map<Character,Integer> c1 = new HashMap<>();
         Map<Character,Integer> c2 = new HashMap<>();
 
+        if(s1.length()>s2.length()) {
+            return false;
+        }
+
         for(char ele:s1.toCharArray()) {
             c1.put(ele, c1.getOrDefault(ele, 0)+1);
         }
 
-        int l = -1;
-        for(int i=0;i<s2.length();i++) {
-            char cur = s2.charAt(i);
+        for(int i=0;i<s1.length();i++) {
+            char ele = s2.charAt(i);
+            c2.put(ele, c2.getOrDefault(ele, 0)+1);
+        }
 
-            if(!c1.containsKey(cur)) {
-                c2.clear();
-                l = -1;
-            } else {
-                if(l == -1) {
-                    l = i;
-                }
-                c2.put(cur, c2.getOrDefault(cur, 0)+1);
-                while(c2.get(cur)>c1.get(cur)) {
-                    c2.put(s2.charAt(l), c2.get(s2.charAt(l))-1);
-                    if(c2.get(s2.charAt(l)) == 0) {
-                        c2.remove(s2.charAt(l));
-                    }
-                    l++;
-                }
-            } 
-
+        for(int i=0;i<s2.length()-s1.length();i++) {
             if(c2.equals(c1)) {
                 return true;
             }
+            char ele = s2.charAt(i+s1.length());
+            c2.put(ele, c2.getOrDefault(ele, 0)+1);
+            c2.put(s2.charAt(i), c2.getOrDefault(s2.charAt(i), 0)-1);
+
+            if (c2.get(s2.charAt(i))==0) {
+                c2.remove(s2.charAt(i));
+            }
+        }
+
+        if(c2.equals(c1)) {
+            return true;
         }
 
         return false;
