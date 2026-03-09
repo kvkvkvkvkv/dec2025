ee2930850639dbd3b390429af6c801489566a25e
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-03-09 11:08:14 +0530

    Time: 7 ms (42.49%) | Memory: 45.1 MB (88.53%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,29 +1,37 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
-        Stack<Integer> stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
 
         for(String ele:tokens) {
-            if(isNum(ele)){
-                stack.add(Integer.valueOf(ele));
-            } else {
-                int num2 = stack.pop();
-                int num1 = stack.pop();
-                if(ele.equals("+")) {
-                    stack.push(num1+num2);
-                } else if(ele.equals("-")) {
-                    stack.push(num1-num2);
-                } else if(ele.equals("*")) {
-                    stack.push(num1*num2);
-                } else {
-                    stack.push(num1/num2);
-                }
+
+            switch (ele) {
+                case "+":
+                    int v1 = Integer.valueOf(stack.pop());
+                    int v2 = Integer.valueOf(stack.pop());
+                    stack.push(v1+v2);
+                    break;
+                case "-":
+                    int v3 = Integer.valueOf(stack.pop());
+                    int v4 = Integer.valueOf(stack.pop());
+                    stack.push(v4-v3);
+                    break;
+                case "/":
+                    int v5 = Integer.valueOf(stack.pop());
+                    int v6 = Integer.valueOf(stack.pop());
+                    stack.push(v6/v5);
+                    break;
+                case "*":
+                    int v7 = Integer.valueOf(stack.pop());
+                    int v8 = Integer.valueOf(stack.pop());
+                    stack.push(v8 *v7);
+                    break;    
+                default:
+                    int v9 = Integer.valueOf(ele);
+                    stack.push(v9);
+                    break;
             }
         }
-        return stack.pop();
-    }
 
-    boolean isNum(String ele) {
-        return !ele.equals("-") && !(ele.equals("+")) && !(ele.equals("*")) && !(ele.equals("/"));
+        return stack.pop();
     }
 }
