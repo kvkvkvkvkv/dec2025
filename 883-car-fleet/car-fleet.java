06de3dfb11403405c284493feac01d2463857f00
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-03-10 06:19:42 +0530

    Time: 64 ms (94.44%) | Memory: 122.1 MB (5.86%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,30 +1,24 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int n = position.length;
-        List<Pair<Integer,Double>> time = new ArrayList();
-        
-        for(int index=0;index<n;index++) {
-            double t = target-position[index];
-            double val = speed[index];
-            double aa = (double)t/val;
-            Pair<Integer,Double> cur = new Pair<>(position[index],aa);
-            time.add(cur);
+        Stack<Double> stack = new Stack<>();
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<position.length;i++) {
+            map.put(position[i],speed[i]);
         }
 
-        Collections.sort(time, (a,b) -> Integer.compare(b.getKey(),a.getKey()));
+        Arrays.sort(position);
 
-        Stack<Pair<Integer,Double>> stack = new Stack();
+        for(int i=position.length-1;i>=0;i--) {
+            double time = (double)(target - position[i])/ map.get(position[i]);
+            
+            if(!stack.isEmpty() && time<=stack.peek()) {
+                continue;
+            }
 
-        int fleet = 0;
-        for(Pair<Integer,Double> cur:time) {
-            while(!stack.isEmpty() && cur.getValue()>stack.peek().getValue()) {
-                stack.pop();
-            }
-            if(stack.isEmpty()) {
-                fleet++;
-            }
-            stack.add(cur);
+            stack.add(time);
         }
-        return fleet;
+        return stack.size();
     }
 }
